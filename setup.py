8bd9daa3393b4c7fc74551103c82b700866a97e1
bpdev75x <bigpump@eclipso.eu>
2025-05-19 15:25:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="solana_sentiment",
+    name="solanasentiment",
     version="0.1",
     packages=find_packages(),
     install_requires=[
